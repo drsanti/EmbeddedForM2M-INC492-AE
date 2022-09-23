050404a4404703a0c359e6578b0cb1c8a001404c
drsanti <santi.inc.kmutt@gmail.com>
2022-09-23 16:10:47 +0700

    update

--- examples/basics/ex03_system_tick/main.c
+++ examples/basics/ex03_system_tick/main.c
@@ -1,14 +1,20 @@
 
 #include <bsp.h>
+#include <rtl.h>
 
 
-void system_tick(void *ticks) {
-	static uint32_t cnt = 0;
-	if(++cnt >= 1000) {
-		cnt = 0;
-		uint16_t x = *((uint16_t *)ticks);
-		Uart1_Printf("ticks: %u\r\n",x);
-	}
+// void system_tick(void *ticks) {
+// 	static uint32_t cnt = 0;
+// 	if(++cnt >= 1000) {
+// 		cnt = 0;
+// 		uint16_t x = *((uint16_t *)ticks);
+// 		Uart1_Printf("ticks: %u\r\n",x);
+// 	}
+	
+// }
+
+void timer_tick() {
+	Uart1_Printf("ticks\r\n");
 	
 }
 
@@ -16,7 +22,8 @@ int main()
 {
 	System_Init();
 
-	Sys_SetTickCallback(system_tick);
+	//Sys_SetTickCallback(system_tick);
+	Timer_Create(1000, timer_tick);
 
 	System_Start();
 }
